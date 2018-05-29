8dbff79c3866c362bc843e1d2b2ae0ed09a5781a
lvfe <lvfe@tcd.ie>
2018-05-29 01:38:10 +0100

    init

---
+++ mypo/src/components/Test.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="test">
+      {{msg}}
+  </div>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to test'
+    }
+  }
+}
+</script>
